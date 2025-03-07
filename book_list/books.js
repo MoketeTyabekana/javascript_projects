b9802110162e17a87f41f1c82cec5743eca67e2d
Mokete <emtyabekana@gmail.com>
2025-03-07 19:33:42 +0200

    Implemented book list display functionality in UI class and update HTML structure

--- book_list/books.js
+++ book_list/books.js
@@ -28,12 +28,26 @@ class UI {
 
     books.forEach((book) => UI.addBookToList(book));
   }
+
+    static addBookToList(book) {
+        const list = document.querySelector("#book-list");
+    
+        const row = document.createElement("tr");
+    
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+    
+        list.appendChild(row);
+    }
 }
 
 // Store Class: Handles Storage.
 
-
-
 // Event: Display Books.
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event: Add a Book.
