cbb10e7f97b071f354037a1ef1e40645bcff235e
Mokete <emtyabekana@gmail.com>
2025-03-12 23:11:41 +0200

    Add conversion output display for Fahrenheit in temperature converter

--- temp-converter/script.js
+++ temp-converter/script.js
@@ -2,4 +2,5 @@ function convert(){
     let celsius=document.getElementById('celsius').value;
 
     let fahrenheit=(celsius*9/5)+32;
+    document.getElementById('results').innerText= fahrenheit;
 }
