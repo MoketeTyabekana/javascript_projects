9aea5280e59d9b2145b21de2819b853f2eb982f4
Mokete <emtyabekana@gmail.com>
2025-03-13 20:08:29 +0200

    Created a new project for random quotes generations.

--- temp-converter/script.js
+++ temp-converter/script.js
@@ -1,6 +1,15 @@
 function convert(){
-    let celsius=document.getElementById('celsius').value;
+    let celsius=parseFloat(document.getElementById('celsius').value);
 
     let fahrenheit=(celsius*9/5)+32;
     document.getElementById('results').innerText= fahrenheit;
 }
+
+
+function clear() {
+    document.getElementById('celsius').value = '';
+    document.getElementById('results').innerText = '';
+}
+
+document.getElementById('clearButton').addEventListener('click', clear);
+
