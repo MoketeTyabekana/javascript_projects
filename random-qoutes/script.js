8bc974923f111b78aa92ff0858dbf6d162eaaeee
Mokete <emtyabekana@gmail.com>
2025-03-13 21:00:24 +0200

    Refactor getQuote function for improved readability and add styling for h2 and button elements

--- random-qoutes/script.js
+++ random-qoutes/script.js
@@ -2,7 +2,5 @@ async function getQuote() {
   let response = await fetch("https://api.quotable.io/random");
   let data = await response.json();
 
-  document.getElementById(
-    "quote"
-  ).innerText = `"${data.content}" - ${data.author}`;
+  document.getElementById("quote").innerText = `"${data.content}" - ${data.author}`;
 }
