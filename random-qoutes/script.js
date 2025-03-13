b127da6d04bdc962b0cfa44ea731f173290b9930
Mokete <emtyabekana@gmail.com>
2025-03-13 20:52:06 +0200

    Display fetched quote and author in getQuote function

--- random-qoutes/script.js
+++ random-qoutes/script.js
@@ -1,4 +1,8 @@
 async function getQuote() {
-     let response= await fetch('https://api.quotable.io/random');
-     let data =await response.json();
+  let response = await fetch("https://api.quotable.io/random");
+  let data = await response.json();
+
+  document.getElementById(
+    "quote"
+  ).innerText = `"${data.content}" - ${data.author}`;
 }
