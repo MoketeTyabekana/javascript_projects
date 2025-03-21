d02ed3f801da66ce42fc1491d14a8021e5f66ddb
Mokete <ematyabekana@gmail.com>
2025-03-21 15:58:59 +0200

    Add event handlers for calculator operations and fix button IDs

---
+++ basic-calculator/script.js
@@ -0,0 +1,22 @@
+
+function add(){
+   
+}
+
+function devide(){
+   
+}
+
+function substact(){
+   
+}
+
+function multiply(){
+   
+}
+
+
+document.getElementById('add','devide','substact','multiply').addEventListener('click',function()
+{
+  
+});
