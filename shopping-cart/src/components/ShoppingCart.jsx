e5a536f4e3e914f4d51b11910319eeaa5fd32254
Mokete <ematyabekana@gmail.com>
2025-04-04 13:54:51 +0200

    Add ShoppingCart component skeleton with initial imports

---
+++ shopping-cart/src/components/ShoppingCart.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { products } from './products.jsx';
+
+const ShoppingCart = () => {
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShoppingCart
