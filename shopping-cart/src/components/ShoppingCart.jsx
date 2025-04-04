e1c7b8d10632f753a7853f415ee9d12e092a2161
Mokete <ematyabekana@gmail.com>
2025-04-04 14:08:50 +0200

    Enhance ShoppingCart title styling with increased font size

--- shopping-cart/src/components/ShoppingCart.jsx
+++ shopping-cart/src/components/ShoppingCart.jsx
@@ -15,7 +15,7 @@ const ShoppingCart = () => {
 const totalPrice=cart.reduce((acc, product) => acc + product.price, 0);
 
   return <div className=" flex flex-column justify-center items-center">
-    <h1 className="p-4 bg-blue-600 w-full text-white font-bold" >Shopping Cart</h1>
+    <h1 className="p-4 bg-blue-600 w-full text-white font-bold text-xl" >Shopping Cart</h1>
 
   </div>;
 };
