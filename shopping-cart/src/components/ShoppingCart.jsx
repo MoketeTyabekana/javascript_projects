b3156dc9382465fb22aa2e022f113f86cb25499d
Mokete <ematyabekana@gmail.com>
2025-04-04 14:41:04 +0200

    Refactor ShoppingCart layout for improved product display and add new products

--- shopping-cart/src/components/ShoppingCart.jsx
+++ shopping-cart/src/components/ShoppingCart.jsx
@@ -12,30 +12,44 @@ const ShoppingCart = () => {
     setCart(cart.filter((product) => product.id !== productId));
   };
 
-const totalPrice=cart.reduce((acc, product) => acc + product.price, 0);
+  const totalPrice = cart.reduce((acc, product) => acc + product.price, 0);
 
-  return <div className=" flex flex-col h-screen bg-gray-50">
-    <h1 className="p-4 bg-blue-600 w-full text-white font-bold text-xl flex justify-start items-center px-10" >Shopping Cart</h1>
+  return (
+    <div className=" flex flex-col h-screen bg-gray-50">
+      <h1 className="p-4 bg-blue-600 w-full text-white font-bold text-xl flex justify-start items-center px-10">
+        Shopping Cart
+      </h1>
 
-    <div className=" p-10 bg-white ">
-     <ul className="flex flex-row md:flex-wrap justify-between items-center gap-10">
-        {products.map((product) => (
-          <li key={product.id} className="flex flex-col h-auto w-1/5 bg-white shadow-lg rounded-lg p-4 mb-4">
-            <div className="flex items-center">
-              <img src={product.image} alt={product.name} className="w-16 h-16 " />
-              <div>
-                <h2 className="text-lg font-bold">{product.name}</h2>
-                <p className="text-gray-600">R{product.price.toFixed(2)}</p>
+      <div className=" p-10 bg-white ">
+        <ul className="grid grid-cols-5 gap-4">
+          {products.map((product) => (
+            <li
+              key={product.id}
+              className="flex flex-col h-auto  bg-white shadow-lg rounded-lg p-4 mb-4"
+            >
+              <div className="flex flex-col  items-center p-4">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="w-25 h-auto "
+                />
+                <div>
+                  <h2 className="text-md font-bold">{product.name}</h2>
+                  <p className="text-gray-600">R{product.price.toFixed(2)}</p>
+                </div>
               </div>
-            </div>
-            <button onClick={() => addToCart(product)} className="bg-blue-500 text-white px-4 py-2 rounded">Add to Cart</button>
-          </li>
-        ))}
-        <li></li>
-     </ul>
+              <button
+                onClick={() => addToCart(product)}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Add to Cart
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-
-  </div>;
+  );
 };
 
 export default ShoppingCart;
--- shopping-cart/src/components/products.jsx
+++ shopping-cart/src/components/products.jsx
@@ -20,6 +20,27 @@ export const products = [
     price: 899.00,
     image: 'https://www.makro.co.za/asset/rukmini/fccp/312/312/ng-fkpublic-ui-user-fbbe/iron/t/c/b/-original-imah3wwwu6b8sfjw.jpeg?q=70',
   },
+
+  {
+    id: 1,
+    name: 'Hisense Fully Automatic Front Load with In-built Heater',
+    price:  4999.00,
+    image: 'https://www.makro.co.za/asset/rukmini/fccp/312/312/ng-fkpublic-ui-user-fbbe/washing-machine/d/i/n/-original-imah2hawtbc5qbzt.jpeg?q=70',
+  },
+
+  {
+    id: 2,
+    name: 'Hisense Dryer',
+    price: 15999.00,
+    image: 'https://www.makro.co.za/asset/rukmini/fccp/312/312/ng-fkpublic-ui-user-fbbe/washing-machine/j/z/3/-original-imah7zgyzyfe6dq6.jpeg?q=70',
+  },
+  {
+    id: 3,
+    name: 'Genesis 426787 Steam Iron',
+    price: 899.00,
+    image: 'https://www.makro.co.za/asset/rukmini/fccp/312/312/ng-fkpublic-ui-user-fbbe/iron/t/c/b/-original-imah3wwwu6b8sfjw.jpeg?q=70',
+  },
+
 ]
  
  
