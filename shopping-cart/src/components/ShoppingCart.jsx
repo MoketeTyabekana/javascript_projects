37599af7d81e5356436ed9c9026822ebfee33ff1
Mokete <ematyabekana@gmail.com>
2025-04-04 14:00:19 +0200

    Implement cart functionality in ShoppingCart component with add and remove features

--- shopping-cart/src/components/ShoppingCart.jsx
+++ shopping-cart/src/components/ShoppingCart.jsx
@@ -1,14 +1,20 @@
-import React, {useState} from 'react';
-import { products } from './products.jsx';
+import React, { useState } from "react";
+import { products } from "./products.jsx";
 
 const ShoppingCart = () => {
+  const [cart, setCart] = useState([]);
 
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
 
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const removeFromCart = (productId) => {
+    setCart(cart.filter((product) => product.id !== productId));
+  };
 
-export default ShoppingCart
+const totalPrice=cart.reduce((acc, product) => acc + product.price, 0);
+
+  return <div></div>;
+};
+
+export default ShoppingCart;
